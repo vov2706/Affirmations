ec6b8febcfaffe11d4010860cd775500820bf895
Volodymyr Yevsiuk <vov2706@gmail.com>
2023-12-18 22:03:43 +0200

    create test

---
+++ app/src/androidTest/java/com/example/affirmations/AffirmationsListTests.kt
@@ -0,0 +1,13 @@
+package com.example.affirmations
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class AffirmationsListTests {
+    @Test
+    fun scroll_to_item() {
+
+    }
+}
